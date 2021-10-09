1ec25f74d45c63ad9727ce4a3918c6f55144ee50
khalil chermiti <khalil666chermiti@gmail.com>
2021-10-09 23:59:46 +0100

    check if number is odd or even

---
+++ oddEven.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    // finds out if number is odd or even
+    int a ;
+    printf("enter a number : ") ;
+    scanf("%i" , &a) ;
+    if(a / 2 == 0) printf("even !") ;
+    else printf("odd !") ;
+}
