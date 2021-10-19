b2d8bf35d18cbcf2be96e8146fdc9877313baa45
khalil chermiti <khalil666chermiti@gmail.com>
2021-10-19 17:46:17 +0100

    program optimization

--- MaxMinMoyNotes.c
+++ MaxMinMoyNotes.c
@@ -6,8 +6,8 @@ void main (){
     printf("entrer le nombre de notes : ") ;
     scanf("%i" , &n);
 
+    int min ;
     int max = 0 ;
-    int min = 0 ;
     int somme = 0 ;
 
     if ((n > 0) && (n <= 20 )) {
@@ -15,9 +15,6 @@ void main (){
         for(int i = 1 ; i <= n ; i++) {
             printf("entrer le nombre numero %i: " , i) ;
             scanf("%i" , &a) ;
-
-            // seulement la premiere repetition
-            if (i == 1) min = a ;
 
             if (max < a) max = a ;
             if (min > a) min = a ;
